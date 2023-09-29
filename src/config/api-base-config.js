436b617a42a474d74d216ee10d22fffbbe10afaf
seifeldin.io <76487753+seifeldinio@users.noreply.github.com>
2023-09-29 17:42:19 +0300

    config: new api url

--- src/config/api-base-config.js
+++ src/config/api-base-config.js
@@ -5,4 +5,5 @@
 // export const BASE_URL = "http://localhost:3309/api/v1";
 
 // Production
-export const BASE_URL = "https://octopus-app-fypbq.ondigitalocean.app/api/v1";
+export const BASE_URL =
+  "https://bloxat-backend-eq8sk.ondigitalocean.app/api/v1";
