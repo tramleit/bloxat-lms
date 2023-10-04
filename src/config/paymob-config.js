14a475ebda4cd85c93ff70166bdf84f455560b29
seifeldin.io <76487753+seifeldinio@users.noreply.github.com>
2023-10-04 22:15:17 +0300

    payment: paymob live id

--- src/config/paymob-config.js
+++ src/config/paymob-config.js
@@ -1,11 +1,22 @@
-// Test mode
+// API Key
 export const PAYMOB_API_KEY =
   "ZXlKMGVYQWlPaUpLVjFRaUxDSmhiR2NpT2lKSVV6VXhNaUo5LmV5SmpiR0Z6Y3lJNklrMWxjbU5vWVc1MElpd2libUZ0WlNJNkltbHVhWFJwWVd3aUxDSndjbTltYVd4bFgzQnJJam94TmpjNE1UQjkuLU1ZYlp0RXdLNTRXR194RkFNZnJ5d0NfbHQ2Zm5vdUdTTU9BN2VxTDAtZFRzZDEzVEFzZ2NwXzg3a0wzNzVTeWJYMm9URDZhYXBDaW01ZXlHQVNwU1E=";
 
-export const CARD_INTEGRATION_ID = 4179213;
+//////////////////////////////////////////////////////////////////
+// Test mode
+// export const CARD_INTEGRATION_ID = 4179213;
+
+// export const IFRAME_ID = 369734;
+
+//////////////////////////////////////////////////////////////////
+// Live mode
+export const CARD_INTEGRATION_ID = 2015311;
 
 export const IFRAME_ID = 369734;
 
-// Live mode
-// export const PAYMOB_API_KEY =
-//   "ZXlKMGVYQWlPaUpLVjFRaUxDSmhiR2NpT2lKSVV6VXhNaUo5LmV5SmpiR0Z6Y3lJNklrMWxjbU5vWVc1MElpd2libUZ0WlNJNkltbHVhWFJwWVd3aUxDSndjbTltYVd4bFgzQnJJam94TmpjNE1UQjkuLU1ZYlp0RXdLNTRXR194RkFNZnJ5d0NfbHQ2Zm5vdUdTTU9BN2VxTDAtZFRzZDEzVEFzZ2NwXzg3a0wzNzVTeWJYMm9URDZhYXBDaW01ZXlHQVNwU1E=";
+//////////////////////////////////////////////////////////////////
+// Transaction processed callback
+// https://bloxat.xyz/api/v1/subscriptions
+
+// Transaction response callback
+// https://teach.bloxat.app/payment-redirect
